330eac5edc131dd936005de20a2e984ea169ee79
Koslun <adam.wamai@gmail.com>
2018-05-05 22:40:17 +0200

    feat(app): add angular2-json-schema-form example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  jsonFormOptions = { addSubmit: false };
+
+  jsonSchemaJson = {
+    type: 'object',
+    properties: {
+      schema: {
+        type: 'array',
+        title: 'Basic information',
+        uniqueItems: true,
+        items: {
+          type: 'object',
+          properties: {
+            name: { title: 'Name of the activity', type: 'string' },
+            description: {
+              title: 'Description of the activity',
+              type: 'string',
+            },
+          },
+        },
+      },
+    },
+  };
+
   constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit(event) {
+    console.log('onSubmit event', event);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {
+  JsonSchemaFormModule,
+  NoFrameworkModule,
+} from 'angular2-json-schema-form';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,6 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+
+    NoFrameworkModule,
+    JsonSchemaFormModule.forRoot(NoFrameworkModule),
   ],
   providers: [],
   bootstrap: [AppComponent],
