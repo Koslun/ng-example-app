3a26a6121c594cfb484d4a7d41d9e0701abd3e4d
Koslun <adam.wamai@gmail.com>
2018-05-05 02:30:45 +0200

    feat: add & boot intercom in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Intercom } from 'ng-intercom';
+
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(
+    public intercom: Intercom
+  ) {}
+
+  ngOnInit() {
+    this.intercom.boot({
+      app_id: environment.intercomAppId,
+      // Supports all optional configuration.
+      widget: {
+        'activator': '#intercom'
+      }
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { IntercomModule } from 'ng-intercom';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    IntercomModule.forRoot({
+      appId: environment.intercomAppId, // from your Intercom config
+      updateOnRouterChange: true // will automatically run `update` on router event changes. Default: `false`
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
