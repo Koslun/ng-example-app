2755e3773366884fe1f9a32271a9f20f9fc12504
Koslun <adam.wamai@gmail.com>
2018-05-09 21:15:25 +0200

    feat(app): add basic upload example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgUploaderModule } from 'ngx-uploader';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, NgUploaderModule],
   providers: [],
   bootstrap: [AppComponent],
 })
